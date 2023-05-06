a02a5f0f2a242d96da473fcbfaff4365e3b6460b
Leo Alfonso Tarigan <117805927+leo-kun19@users.noreply.github.com>
2023-05-06 23:48:44 +0700

    Initialize php-8.1

---
+++ README.md
@@ -0,0 +1,52 @@
+
+# Laravel Multi Auth
+
+The Laravel Framework project boilerplate with Inertia setup in vue. It has separate model and guards for both user and admin.
+
+
+## Run Locally
+
+Clone the project
+
+```bash
+git clone https://github.com/itxshakil/laravel-multi-auth
+```
+
+Go to the project directory
+
+```bash
+cd laravel-multi-auth
+```
+
+Install dependencies & build assets
+
+```bash
+composer install
+npm install && npm run dev
+```
+
+Migrate and seed database
+
+```bash
+php artisan migrate
+```
+
+Start the server
+
+```bash
+php artisan serve
+```
+
+
+## Running Tests
+
+To run tests, run the following command
+
+```bash
+  php artisan test
+```
+
+
+## License
+
+This package inherits the licensing of its parent framework, Laravel, and as such is open-sourced software licensed under the [MIT](https://choosealicense.com/licenses/mit/)
---
+++ app/Exceptions/Handler.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Exceptions;
+
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\RedirectResponse;
+use Throwable;
+
+class Handler extends ExceptionHandler
+{
+    /**
+     * A list of exception types with their corresponding custom log levels.
+     *
+     * @var array<class-string<\Throwable>, \Psr\Log\LogLevel::*>
+     */
+    protected $levels = [
+        //
+    ];
+
+    /**
+     * A list of the exception types that are not reported.
+     *
+     * @var array<int, class-string<\Throwable>>
+     */
+    protected $dontReport = [
+        //
+    ];
+
+    /**
+     * A list of the inputs that are never flashed to the session on validation exceptions.
+     *
+     * @var array<int, string>
+     */
+    protected $dontFlash = [
+        'current_password',
+        'password',
+        'password_confirmation',
+    ];
+
+    /**
+     * Register the exception handling callbacks for the application.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->reportable(function (Throwable $e) {
+            //
+        });
+    }
+
+    /**
+     * Convert an authentication exception into a response.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Auth\AuthenticationException  $exception
+     * @return \Illuminate\Http\JsonResponse|\Illuminate\Http\RedirectResponse
+     */
+    protected function unauthenticated($request, AuthenticationException $exception): JsonResponse|RedirectResponse
+    {
+        $guard = collect($exception->guards())->first();
+        $route = match ($guard) {
+            'admin' => 'admin.login',
+            default => 'login',
+        };
+
+        return $request->expectsJson()
+            ? response()->json(['message' => $exception->getMessage()], 401)
+            : redirect()->guest(route($route));
+    }
+}
